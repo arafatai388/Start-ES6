daa713dc6d837247b1bb10a4323bfa98708fbfc2
arafatai388 <arafat.cseai@gmail.com>
2026-08-01 21:54:00 +0600

    Understand var,let and constant

---
+++ 1.var_let_const.js
@@ -0,0 +1,25 @@
+// ===============================
+// var Example
+// ===============================
+
+var age;
+age = 22;
+
+console.log("Age:", age);
+
+// ===============================
+// let Example
+// ===============================
+
+let name;
+name = "Arafat";
+
+console.log("Name:", name);
+
+// ===============================
+// const Example
+// ===============================
+
+const country = "Bangladesh";
+
+console.log("Country:", country);
